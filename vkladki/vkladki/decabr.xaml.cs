606a953d736b64d16d9a65a23476ec2840b86b45
opilane <opilane@HP-CZC6297B9H>
2020-10-19 14:30:35 +0300

    Add project files.

---
+++ vkladki/vkladki/decabr.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace vkladki
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class decabr : ContentPage
+    {
+        public decabr()
+        {
+            InitializeComponent();
+            Grid grd = new Grid
+            {
+                RowDefinitions =
+   {
+   new RowDefinition {Height=new GridLength(1,GridUnitType.Star)},
+   new RowDefinition {Height=new GridLength(1,GridUnitType.Star)},
+   new RowDefinition {Height=new GridLength(1,GridUnitType.Star)}
+   },
+                ColumnDefinitions =
+   {
+   new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) }
+   }
+            };
+            Label nimetus = new Label { Text = "Маквин", FontSize = 30 };
+            Image img = new Image { Source = "makvin.jpg" };
+            Label kirjeldus = new Label { Text = "САМАЯ БЫСТРАЯ," +
+                " САМАЯ КЛАССНАЯ," +
+                " САМАЧ КРАСИВАЯ" };
+            var tap = new TapGestureRecognizer();
+            tap.Tapped += async (s, e) =>
+            {
+                img = (Image)s;
+                await DisplayAlert("Информация", "КЧАУ", "Закрыть");
+            };
+            img.GestureRecognizers.Add(tap);
+            grd.Children.Add(nimetus, 0, 0);
+            grd.Children.Add(img, 0, 1);
+            grd.Children.Add(kirjeldus, 0, 2);
+            Content = grd;
+        }
+    }
+}
